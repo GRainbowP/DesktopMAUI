c0876f2f825982b7ade5b79e240ee125311fc697
GRainbowP <uT_3775@zsp3zamosc.pl>
2025-05-22 09:32:12 +0200

    Initial commit

---
+++ DekstopMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DekstopMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DekstopMAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DekstopMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
